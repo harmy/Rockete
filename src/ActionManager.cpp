8e94b2cef91afc084d72af6b506e5bd8f7e5df53
gogoprog <gogoprog@00c138e4-e653-9487-3a91-a3af855835fa>
2011-09-14 22:44:28 +0000

    Missing file added.
    git-svn-id: http://rockete.googlecode.com/svn/trunk@9 00c138e4-e653-9487-3a91-a3af855835fa

---
+++ src/ActionManager.cpp
@@ -0,0 +1,46 @@
+#include "ActionManager.h"
+
+
+ActionManager::ActionManager()
+{
+
+}
+
+ActionManager::~ActionManager()
+{
+    for(int i=0;i<previousActionList.size();++i)
+    {
+        delete previousActionList[i];
+    }
+
+    for(int i=0;i<nextActionList.size();++i)
+    {
+        delete nextActionList[i];
+    }
+}
+
+void ActionManager::applyNew(Action * action)
+{
+    action->apply();
+    previousActionList.push_back(action);
+}
+
+void ActionManager::applyPrevious()
+{
+    if(previousActionList.size())
+    {
+        Action *action = previousActionList.takeLast();
+        action->unapply();
+        nextActionList.push_back(action);
+    }
+}
+
+void ActionManager::applyNext()
+{
+    if(nextActionList.size())
+    {
+        Action *action = nextActionList.takeLast();
+        action->apply();
+        previousActionList.push_back(action);
+    }
+}
